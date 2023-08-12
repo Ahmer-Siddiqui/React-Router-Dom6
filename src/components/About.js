931a6e391f12ecabced974943b71135a9c4d9c08
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-08-12 18:34:19 +0500

    put Link for navigation

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div>
-      <h1>
-            About Page
-      </h1>
+      <h1>About Page</h1>
+      <p>This is a About page of our awesome app</p>
+      <p>And here we are learning about router</p>
+      <Link to="/">Go to Home Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-        <h1>
-            Home Page
-        </h1>
+        <h1>Home Page</h1>
+        <p>This is a home page of our awesome app</p>
+        <p>And here we are learning about router</p>
+        <Link to="/about">Go to About Page</Link>
     </div>
   )
 }
