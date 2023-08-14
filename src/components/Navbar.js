e9a6db34f486cd4ce0ba9907579ff4490c4c107e
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-08-14 18:44:29 +0500

    Active Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,46 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
-
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  
   return (
     <div>
-        <ul className='navbar'>
-            <li><NavLink className='nav-bar-link' style={{backgroundColor: 'red'}} to="/">Home</NavLink></li>
-            <li><NavLink className='nav-bar-link' to="/about">About</NavLink></li>
-            <li><NavLink className='nav-bar-link' to="/contact">Contact Us</NavLink></li>
-           
-        </ul>
-      
-      
+      <ul className="navbar">
+        <li>
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => {
+              return { color: isActive ? "skyblue" : "" };
+            }}
+            className="nav-bar-link"
+            to="/contact"
+          >
+            Contact Us
+          </NavLink>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
